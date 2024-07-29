a83b2898de8d2b6e165cfcaa174ed2668c201bc0
ufidon <ufidon@gmail.com>
2024-07-29 13:31:13 -0400

    graph node touching times and order

--- mod4/demos/gtm.cpp
+++ mod4/demos/gtm.cpp
@@ -12,10 +12,11 @@ public:
 
   void addEdge(T src, T dest);
 
-  void BFT(T start);
-  void DFT(T start);
+  void BFT(T start, std::vector<int> &vn, std::vector<int> &vp);
+  void DFT(T start, std::vector<int> &vn, std::vector<int> &vp);
 
-private:
+  void print();
+
   int vertices;
   std::vector<std::vector<int>> adjMatrix;
   std::unordered_map<T, int> vertexIndexMap;
@@ -54,14 +55,17 @@ void Graph<T>::addEdge(T src, T dest)
 }
 
 template <typename T>
-void Graph<T>::BFT(T start)
+void Graph<T>::BFT(T start, std::vector<int> &vn, std::vector<int> &vp)
 {
   std::vector<bool> visited(vertices, false);
+  vn = std::vector<int>(vertices, 0);
   std::queue<int> queue;
 
   int startIndex = getVertexIndex(start);
   visited[startIndex] = true;
+  vp.push_back(startIndex);
   queue.push(startIndex);
+  vn[startIndex]++;
 
   while (!queue.empty())
   {
@@ -71,6 +75,12 @@ void Graph<T>::BFT(T start)
 
     for (int i = 0; i < vertices; ++i)
     {
+      if (adjMatrix[vertexIndex][i] == 1)
+      {
+        vn[i]++;
+        vp.push_back(i);
+      }
+
       if (adjMatrix[vertexIndex][i] == 1 && !visited[i])
       {
         visited[i] = true;
@@ -82,9 +92,10 @@ void Graph<T>::BFT(T start)
 }
 
 template <typename T>
-void Graph<T>::DFT(T start)
+void Graph<T>::DFT(T start, std::vector<int> &vn, std::vector<int> &vp)
 {
   std::vector<bool> visited(vertices, false);
+  vn = std::vector<int>(vertices, 0);
   std::stack<int> stack;
 
   int startIndex = getVertexIndex(start);
@@ -99,10 +110,17 @@ void Graph<T>::DFT(T start)
     {
       std::cout << indexVertexMap[vertexIndex] << " ";
       visited[vertexIndex] = true;
+      vp.push_back(vertexIndex);
+      vn[vertexIndex]++;
     }
 
     for (int i = 0; i < vertices; ++i)
     {
+      if (adjMatrix[vertexIndex][i] == 1)
+      {
+        vp.push_back(i);
+        vn[i]++;
+      }
       if (adjMatrix[vertexIndex][i] == 1 && !visited[i])
       {
         stack.push(i);
@@ -112,22 +130,61 @@ void Graph<T>::DFT(T start)
   std::cout << std::endl;
 }
 
+template <typename T>
+void Graph<T>::print()
+{
+  for (size_t i = 0; i < vertices; i++)
+  {
+    for (size_t j = 0; j < vertices; j++)
+    {
+      std::cout << adjMatrix[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
-  Graph<std::string> graph(6);
+  int nvertices = 6;
+  Graph<std::string> graph(nvertices);
 
   graph.addEdge("A", "B");
   graph.addEdge("A", "C");
   graph.addEdge("B", "D");
   graph.addEdge("B", "E");
   graph.addEdge("C", "F");
-  graph.addEdge("E", "F");
+  // graph.addEdge("E", "F");
+  graph.print();
 
   std::cout << "Breadth-First Traversal (BFT) starting from vertex A:\n";
-  graph.BFT("A");
+  std::vector<int> vn, vp, vn2, vp2;
+  graph.BFT("A", vn, vp);
+  for (size_t i = 0; i < nvertices; i++)
+  {
+    std::cout << graph.indexVertexMap[i] << "(" << vn[i] << ") ";
+  }
+  std::cout << std::endl;
+
+  for (auto p : vp)
+  {
+    std::cout << graph.indexVertexMap[p] << " ";
+  }
+  std::cout << std::endl;
 
   std::cout << "Depth-First Traversal (DFT) starting from vertex A:\n";
-  graph.DFT("A");
+  graph.DFT("A", vn2, vp2);
+  for (size_t i = 0; i < nvertices; i++)
+  {
+    std::cout << graph.indexVertexMap[i] << "(" << vn2[i] << ") ";
+  }
+  std::cout << std::endl;
+
+  for (auto p : vp2)
+  {
+    std::cout << graph.indexVertexMap[p] << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
